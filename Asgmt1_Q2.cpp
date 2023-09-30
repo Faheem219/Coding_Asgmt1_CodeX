86e36eb19ce5112bc178ec09145d38c26d592bc7
Faheem <faheemuddinsayyed789@gmail.com>
2023-09-30 21:48:18 +0530

    Minor Change

--- Asgmt1_Q2.cpp
+++ Asgmt1_Q2.cpp
@@ -6,8 +6,7 @@ int main(){
     cout<<"Range of char: "<<CHAR_MIN<<" to "<<CHAR_MAX<<endl;
     cout<<"Range of short int: "<<SHRT_MIN<<" to "<<SHRT_MAX<<endl;
     cout<<"Range of long int: "<<LONG_MIN<<" to "<<LONG_MAX<<endl;
-    cout<<"Range of double: "<<numeric_limits<double>::min()<<\
-    " to "<<numeric_limits<double>::max()<<endl;
+    cout<<"Range of double: "<<__DBL_MIN__<<" to "<<__DBL_MAX__<<endl;
 
     return 0;
     
